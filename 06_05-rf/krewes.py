fea6f8675af0a3c3eb3733d3e83efe4609f27db7
ichenadamczyk20 <48662100+ichenadamczyk20@users.noreply.github.com>
2020-10-02 07:39:22 -0400

    Update krewes.py

--- 06_05-rf/krewes.py
+++ 06_05-rf/krewes.py
@@ -5,7 +5,7 @@
 
 import random
 
-# A dictionary storing team members in the format <team name> -> [<member list>]
+# A dictionary storing team members in the format <team name> -> [member list]
 KREWES = {
     'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
     'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
@@ -14,9 +14,9 @@ KREWES = {
 
 # Asks user for a team name and ensures the input is lowercase
 team = input("Pick a team from orpheus, rex, or endymion: ").lower()
-# Checks if the input is an existing team's name.
+# Checks if the input is an existing team's name
 # Until it is, it will ask the user for a team name again.
 while team not in KREWES: 
     team = input("Please enter either orpheus, rex, or endymion: ").lower()
-# Selects and outputs a random team member from the given team name.
+# Selects and outputs a random team member from the given team name
 print(random.choice(KREWES[team]))
