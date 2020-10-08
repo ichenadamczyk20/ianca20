2592de403423aaaf9f39950a15bd1b5942543323
ichenadamczyk20 <48662100+ichenadamczyk20@users.noreply.github.com>
2020-10-08 15:10:16 -0400

    Update app.py

--- 10_flask01/occupy_flask_st/app.py
+++ 10_flask01/occupy_flask_st/app.py
@@ -12,7 +12,7 @@ def hello_world():
     heading = "Team Kent Cockroaches (William Yin, Arib Chowdhury, Ian Chen-Adamczyk)<br>SoftDev<br>K10 -- Putting Little Pieces Together<br>2020-10-09"
     occupation_dict = occupations.build_dict('occupations.csv')
     occupation_select = occupations.select_occupation(occupation_dict)
-    return f"{heading}<br><br>{occupation_select}<br><br>{str(occupation_dict).replace(',', ',<br>')}"
+    return f"{heading}<br><br>Your chosen occupation is {occupation_select}<br><br>{'<br>'.join(list(occupation_dict.keys())[:-1])}"
 
 if __name__ == "__main__":
     app.debug = True
