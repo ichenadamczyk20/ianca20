24c3b7a5daf38e63421b26655b491726ceff8de6
ichenadamczyk20 <48662100+ichenadamczyk20@users.noreply.github.com>
2020-10-08 15:03:22 -0400

    Update app.py

--- 10_flask01/occupy_flask_st/app.py
+++ 10_flask01/occupy_flask_st/app.py
@@ -9,15 +9,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    roster = """Team Kent Cockroaches (William Yin, Arib Chowdhury, Ian Chen-Adamczyk)
-<br>SoftDev
-<br>K10 -- Putting Little Pieces Together
-<br>2020-10-09"""
-    occupationDict = occupations.build_dict('occupations.csv')
-    occupationSelect = occupations.select_occupation(occupationDict)
-    return f"{roster}<br><br> \
-            {occupationSelect}<br><br> \
-            {str(occupationDict).replace(',', ',<br>')}"
+    heading = "Team Kent Cockroaches (William Yin, Arib Chowdhury, Ian Chen-Adamczyk)<br>SoftDev<br>K10 -- Putting Little Pieces Together<br>2020-10-09"
+    occupation_dict = occupations.build_dict('occupations.csv')
+    occupation_select = occupations.select_occupation(occupation_dict)
+    return f"{heading}<br><br>{occupation_select}<br><br>{str(occupation_dict).replace(',', ',<br>')}"
 
 if __name__ == "__main__":
     app.debug = True
